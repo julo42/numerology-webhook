c3d2782c8fc569b80332ac42442d920291a53a41
Julien Olivier <julo42@gmail.com>
2026-02-01 07:48:30 +0100

    Version avec jobs persistants 7

--- webhook.py
+++ webhook.py
@@ -194,4 +194,4 @@ if __name__ == "__main__":
         t.start()
 
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
